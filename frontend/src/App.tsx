078d7f436f09d9d5ebd9c282c2b42cf5d50aa845
DanielJohn17 <danielyohannes38@gmail.com>
2025-04-22 18:01:56 +0300

    shadcn setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,14 @@
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex flex-col">
-        <button
-          className="bg-blue-100"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Up
-        </button>
-        <button
-          className="bg-red-100"
-          onClick={() => setCount((count) => count - 1)}
-        >
-          Down
-        </button>
+      <div className="flex flex-col max-w-2xs m-auto gap-y-5">
+        <Button onClick={() => setCount((count) => count + 1)}>Up</Button>
+        <Button onClick={() => setCount((count) => count - 1)}>Down</Button>
 
         <p>{count}</p>
       </div>
