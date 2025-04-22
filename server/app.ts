defe735ef9edd41329cc2247fece6b85ef315ad7
DanielJohn17 <danielyohannes38@gmail.com>
2025-04-22 21:06:14 +0300

    added cors

--- server/app.ts
+++ server/app.ts
@@ -2,8 +2,11 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { expensesRoute } from './routes/expenses';
 import { serveStatic } from 'hono/bun';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
+
+app.use('/api/*', cors());
 
 app.use('*', logger());
 
