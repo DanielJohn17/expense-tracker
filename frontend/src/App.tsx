52af77f0a0c50661fa00c8d48cd4321ee6f402fa
DanielJohn17 <danielyohannes38@gmail.com>
2025-04-22 19:55:46 +0300

    fetching from api using vite proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
@@ -9,6 +9,17 @@ import {
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    const fetchTotal = async () => {
+      const res = await fetch('/api/expenses/total-spent');
+      const data = await res.json();
+
+      setTotalSpent(data.total);
+    };
+
+    fetchTotal();
+  }, []);
 
   return (
     <Card className="w-[350px] m-auto">
