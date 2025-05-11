435108b8e981d657ccfd1664fb8ffd8afe569798
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-12 02:12:06 +0300

    created userQueryOptions

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,9 @@
-import { hc } from 'hono/client';
-import { type ApiRoutes } from '@server/app';
+import { hc } from "hono/client";
+import { type ApiRoutes } from "@server/app";
+import { queryOptions } from "@tanstack/react-query";
 
-const url = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const url = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
-const client = hc<ApiRoutes>(`${url}`);
 // Custom fetch function to include credentials
 const customFetch = ((input: RequestInfo | URL, init?: RequestInit) => {
   return fetch(input, { ...init, credentials: "include" });
@@ -12,3 +12,19 @@ const customFetch = ((input: RequestInfo | URL, init?: RequestInit) => {
 const client = hc<ApiRoutes>(`${url}`, { fetch: customFetch });
 
 export const api = client.api;
+
+const getCurrentUser = async () => {
+  const res = await api.me.$get();
+
+  if (!res.ok) {
+    throw new Error("Server Error");
+  }
+  const data = await res.json();
+  return data;
+};
+
+export const userQueryOptions = queryOptions({
+  queryKey: ["get-current-user"],
+  queryFn: getCurrentUser,
+  staleTime: Infinity,
+});
