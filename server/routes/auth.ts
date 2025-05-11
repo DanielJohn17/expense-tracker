e0ebbe3b3e41833c7a3ed536ff64eebfae1ecc00
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-12 02:09:36 +0300

    used getUser middleware to check if user is authenticated

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
-import { kindeClient, sessionManager } from "../kinde";
-import { error } from "console";
+import { getUser, kindeClient, sessionManager } from "../kinde";
 
 export const authRoute = new Hono()
   .get("/login", async (c) => {
@@ -20,14 +19,7 @@ export const authRoute = new Hono()
     const logoutUrl = await kindeClient.logout(sessionManager(c));
     return c.redirect(logoutUrl.toString());
   })
-  .get("/me", async (c) => {
-    const manager = sessionManager(c);
-    const isAuthenticated = await kindeClient.isAuthenticated(manager);
-    if (!isAuthenticated) {
-      return c.json({ error: "unauthorized" }, 401);
-    }
-
-    const user = await kindeClient.getUserProfile(manager);
-
-    return c.json(user);
+  .get("/me", getUser, async (c) => {
+    const user = c.var.user;
+    return c.json({ user });
   });
