361602a0695156a93aa20fa5590b5cd35ed64733
DanielJohn17 <danielyohannes38@gmail.com>
2025-05-12 02:11:01 +0300

    created custom fetch function to include credentials

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,5 +4,11 @@ import { type ApiRoutes } from '@server/app';
 const url = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 const client = hc<ApiRoutes>(`${url}`);
+// Custom fetch function to include credentials
+const customFetch = ((input: RequestInfo | URL, init?: RequestInit) => {
+  return fetch(input, { ...init, credentials: "include" });
+}) as typeof fetch;
+
+const client = hc<ApiRoutes>(`${url}`, { fetch: customFetch });
 
 export const api = client.api;
